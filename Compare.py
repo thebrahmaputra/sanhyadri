d77618768b2feaf436f69d2decaa8084289a8c1f
mepratikbidwai <mepratikbidwai@gmail.com>
2018-07-29 16:03:50 +0000

    Compare two numbers

---
+++ Compare.py
@@ -0,0 +1,12 @@
+#print ("Pratik");
+print("Pratik");
+cnt = 20;
+inpnum = int(input("Input your number:"));
+#print("Your value is:" +inpnum+ " ");
+print("Your value is : %d" %inpnum)
+if inpnum > cnt :
+    print("%d is greater than %d" %(inpnum, cnt));
+elif inpnum < cnt :
+    print("%d is lesser than %d" %(inpnum, cnt));
+else :
+    print("both numbers are equal");
